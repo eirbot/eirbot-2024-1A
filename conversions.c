6e8a1a9b59778ee691638b64ac075e519b2eb834
HORRUT Thomas <thomas.horrut@bordeaux-inp.fr>
2023-11-29 18:52:03 +0100

    🏗correct skeleton

--- conversions.c
+++ conversions.c
@@ -1,66 +1,64 @@
 #include <stdio.h>
 
-float compt_long(int n1, int n2)
+#define d1 0.05  // in meters
+#define r 0.05 // in meters
+
+/**
+    Interacts with the motors to rotates the two wheels with the specified rotation angles.
+*/
+void accomplish_steps(const float *left_wheel_rotation, const float *right_wheel_rotation)
 {
-	float r = 1, resolution = 1;
-	float lambda = resolution * r/2 * (n1 + n2);
-	return lambda ;
+
 }
 
-float compt_rot(int n1, int n2)
+/**
+    Assuming the left and right wheels rotate with the same angular speed, 
+    sets their rotation to cross the specified distance. 
+*/
+void set_wheels_rotation_from_distance(float distance, float *left_wheel_rotation, float *right_wheel_rotation)
 {
-	float r = 1, d = 1, resolution = 1;
-	float rot = resolution * r/(2*d) * (n1 - n2);
-	return rot ;
-}
-
-float consigne_moteur_from_longueur(float distance)
-{}
-
-float consigne_moteur_from_rotation(float angle_rotation)
-{}
-
-void consigne_moteur_gauche(float consigne)
-{}
-
-void consigne_moteur_droite(float consigne)
-{}
-
-void avancer(float consigne_distance)
-{
-	float distance = consigne_distance;
-	float consigne_moteur_avancer = consigne_moteur_from_longueur(consigne_distance); //fonction qui prend en argument la distance que doit parcourir le robot et renvoie l'angle (ou nombre de pas) que doit faire les moteur avec l'hypothese teta_gauche = teta_droite
-	consigne_moteur_gauche(consigne_moteur_avancer); //fonction qui prend en argument l'angle que doit faire le moteur gauche et instruit le moteur 
-	consigne_moteur_droite(consigne_moteur_avancer); //pareil pour le moteur droit
-}
-
-void rotation(float consigne_rotation)
-{
-	float angle_rotation = consigne_rotation;
-	float consigne_moteur_tourner = consigne_moteur_from_rotation(angle_rotation); //fonction qui prend en argument l'angle de rotation du robot et renvoie l'angle (ou nombre de pas) que doit faire les moteur avec l'hypothese teta_gauche = -+teta_droite
-		if (angle_rotation < 0)
-		{
-			consigne_moteur_gauche(-1*consigne_moteur_tourner); //fonction qui prend en argument l'angle que doit faire le moteur gauche et instruit le moteur 
-			consigne_moteur_droite(consigne_moteur_tourner); //pareil pour le moteur droit
-		}
-		else 
-		{
-			consigne_moteur_gauche(consigne_moteur_tourner); //fonction qui prend en argument l'angle que doit faire le moteur gauche et instruit le moteur 
-			consigne_moteur_droite(-1*consigne_moteur_tourner); //pareil pour le moteur droit
-		}
-}
-
-void command(char * avancer_ou_tourner[], float consigne_distance_ou_rotation)
-{
-	if (avancer_ou_tourner = "avance")
-	{
-		avancer(consigne_distance_ou_rotation);
-	}
-	else 
-	{
-		rotation(consigne_distance_ou_rotation);
-	}
+    if (distance) {
+        *left_wheel_rotation = distance/r;
+        *right_wheel_rotation = *left_wheel_rotation;
+    } else {
+        *left_wheel_rotation = 0;
+        *right_wheel_rotation = 0;
+    }
 }
 
 
-int main(){}
+/**
+    Carries out a rectilign forward move with the specified distance (metres).
+*/
+void advance(float distance)
+{
+    float left_wheel_rotation;
+    float right_wheel_rotation;
+	set_wheels_rotation_from_distance(distance, &left_wheel_rotation, &right_wheel_rotation);
+    accomplish_steps(&left_wheel_rotation, &right_wheel_rotation);
+}
+
+/**
+    Assuming the left and right wheels rotate with opposite angular speeds,
+    sets their rotation to rotates the specified angle.
+*/
+void set_wheels_rotation_from_global_rotation(float global_rotation, float *wheel_left_rotation, float *wheel_right_rotation) {
+    if (global_rotation) {
+        *wheel_left_rotation = d1*global_rotation/r;
+        *wheel_right_rotation = -(*wheel_left_rotation);    
+    } else {
+        *wheel_left_rotation = 0;
+        *wheel_right_rotation = 0;
+    }
+}
+
+/**
+    Rotates the robot with the specified angle (radian). A positive angle means a clock-wise one.
+*/
+void rotate(float global_rotation)
+{
+    float left_wheel_rotation;
+    float right_wheel_rotation;
+	set_wheels_rotation_from_global_rotation(global_rotation, &left_wheel_rotation, &right_wheel_rotation);
+    accomplish_steps(&left_wheel_rotation, &right_wheel_rotation);
+}
