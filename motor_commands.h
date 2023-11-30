f48ca575ad564fb62fdaa44a1bb99f8a1465d9ff
Thomas Wanchai MENIER <tmenier@enseirb-matmeca.fr>
2023-11-30 17:38:21 +0100

    fix: typo badly copy pasted

--- motor_commands.h
+++ motor_commands.h
@@ -1,5 +1,5 @@
 // This file handles the connection to the motors and any low-level
 // commands that must be sent to the motors
 
-void set_wheels_rotation_from_global_rotation(float theta_rad, float *wheel_left_rotation, float *wheel_right_rotation);
+void set_wheels_rotation_from_distance(float distance, float *left_wheel_rotation, float *right_wheel_rotation);
 void set_wheels_rotation_from_global_rotation(float theta_rad, float *wheel_left_rotation, float *wheel_right_rotation);
