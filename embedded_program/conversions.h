62e70e9f2326521a8c1ac85c76bcef9ee6ce4c5b
HORRUT Thomas <thomas.horrut@bordeaux-inp.fr>
2023-12-22 18:10:04 +0100

    🔧 correct constants for step calculation

--- embedded_program/conversions.h
+++ embedded_program/conversions.h
@@ -18,7 +18,7 @@ void set_wheels_rotation_from_global_rotation(float theta_rad, struct wheel_step
 /**
  * Assuming the left and right wheels rotate with the same angular speed,
  *  sets their rotation to cross the specified distance.
- * @param distance The distance to travel, in meters 
+ * @param distance The distance to travel, in centimetres
  * @param left_wheel_step_data Pointer to the left wheel step number var
  * @param right_wheel_step_data Pointer to the right wheel step number var
  */
--- embedded_program/conversions.ino
+++ embedded_program/conversions.ino
@@ -1,15 +1,15 @@
 #include "conversions.h"
 
-#define d1 0.05
-#define r 0.05
-#define lambda 100 // step conversion
+#define AXLE_TREE_LENGTH 13.1 // centimetres
+#define WHEEL_RADIUS 3.65 // centimetres
+#define STEPS_BY_RAD 31.83098861 // steps/rad
 
 
 void set_wheels_rotation_from_global_rotation(float theta_rad, struct wheel_step_data *left_wheel_step_data, struct wheel_step_data *right_wheel_step_data)
 {
     if (theta_rad) {
-        float rotation = d1*theta_rad/r;
-        left_wheel_step_data->step_number = (rotation>=0)?(lambda*rotation):(-lambda*rotation);
+        float rotation = AXLE_TREE_LENGTH*theta_rad/WHEEL_RADIUS;
+        left_wheel_step_data->step_number = (rotation>=0)?(STEPS_BY_RAD*rotation):(-STEPS_BY_RAD*rotation);
         left_wheel_step_data->direction = (rotation>=0);
         right_wheel_step_data->step_number = left_wheel_step_data->step_number;
         right_wheel_step_data->direction = !(left_wheel_step_data->direction);
@@ -22,8 +22,8 @@ void set_wheels_rotation_from_global_rotation(float theta_rad, struct wheel_step
 void set_wheels_rotation_from_distance(float distance, struct wheel_step_data *left_wheel_step_data, struct wheel_step_data *right_wheel_step_data)
 {
     if (distance) {
-        float rotation = distance/r;
-        left_wheel_step_data->step_number = (rotation>=0)?(lambda*rotation):(-lambda*rotation);;
+        float rotation = distance/WHEEL_RADIUS;
+        left_wheel_step_data->step_number = (rotation>=0)?(STEPS_BY_RAD*rotation):(-STEPS_BY_RAD*rotation);;
         left_wheel_step_data->direction = (rotation>=0);
         *right_wheel_step_data = *left_wheel_step_data;
     } else {
