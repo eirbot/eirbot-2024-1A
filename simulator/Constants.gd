66701d23da3af9ef2fb0095d73e0491fb272cd4e
Convolutio <79534557+Convolutio@users.noreply.github.com>
2024-03-16 22:16:50 +0100

    ✨ 🚧 autoput plants

--- simulator/Constants.gd
+++ simulator/Constants.gd
@@ -1,21 +1,22 @@
 extends Node
 
+# All the units are mm
+
 @export var TABLE_SHAPE = Vector2i(3000, 2000);
-@export var SOLAR_PANELS: Array[Vector2i] = [Vector2i(275, 2050), Vector2i(500, 2050), Vector2i(725, 2050),
-Vector2i(1275, 2050), Vector2i(1500, 2050), Vector2i(1725, 2050),
-Vector2i(2275, 2050), Vector2i(2500, 2050), Vector2i(2725, 2050)];
+@export var SOLAR_PANELS: Array[Vector2i] = [
+	Vector2i(275, 2050), Vector2i(500, 2050), Vector2i(725, 2050),
+	Vector2i(1275, 2050), Vector2i(1500, 2050), Vector2i(1725, 2050),
+	Vector2i(2275, 2050), Vector2i(2500, 2050), Vector2i(2725, 2050)
+	];
 @export var SOLAR_PANEL_SIDE_SIZE: int = 100;
-@export var POT_RADIUS: int = 250
-@export var PLANTS: Array[Vector2i] = [Vector2i(1300, 1000), Vector2i(700, 1000), Vector2i(1500, 1500),
-					  Vector2i(500, 1500), Vector2i(1300, 2000), Vector2i(700, 2000)]
-@export var OBSTACLE_RADIUS: int = 250
-@export var OBSTACLE_AREAS: Array[Vector2i] = [Vector2i(1387, 35), Vector2i(612, 35), Vector2i(1965, 1000),
-							  Vector2i(1965, 2000), Vector2i(1387, 2965), Vector2i(612, 2965)]
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+@export var POT_SPOT_RADIUS: int = 125;
+@export var POT_RADIUS: int = 60;
+@export var PLANTS: Array[Vector2i] = [
+	Vector2i(1000, 1300), Vector2i(1000, 700), Vector2i(1500, 1500),
+	Vector2i(1500, 500), Vector2i(2000, 1300), Vector2i(2000, 700),
+	];
+@export var OBSTACLE_RADIUS: int = 250;
+@export var OBSTACLE_AREAS: Array[Vector2i] = [
+	Vector2i(1387, 35), Vector2i(612, 35), Vector2i(1965, 1000),
+	Vector2i(1965, 2000), Vector2i(1387, 2965), Vector2i(612, 2965)
+	];
--- simulator/gen_table.gd
+++ simulator/gen_table.gd
@@ -1,18 +1,33 @@
 extends Node2D
 
 @export var solar_panel_scene: PackedScene
-@onready var TABLE_RATIO = $Constants.TABLE_SHAPE.x * 1.0 /$TextureRect.size.x # cm.px⁻¹
+@export var plant_scene: PackedScene
+@onready var TABLE_RATIO: float = $Constants.TABLE_SHAPE.x * 1.0 / \
+												$TextureRect.size.x # mm.px⁻¹
+
+func rescale(sprite: Sprite2D, expected_cm_size: Vector2i) -> void:
+	sprite.set_scale((expected_cm_size*1.0) / (TABLE_RATIO * \
+													sprite.get_rect().size));
 
 func place_elts():
 	for coord: Vector2i in $Constants.SOLAR_PANELS:
 		var new_solar_panel: Sprite2D = solar_panel_scene.instantiate();
-		var side_scale: float = ($Constants.SOLAR_PANEL_SIDE_SIZE*1.0) / (TABLE_RATIO * new_solar_panel.get_rect().size.x);
-		new_solar_panel.scale = Vector2(side_scale, side_scale);
+		rescale(new_solar_panel, Vector2i($Constants.SOLAR_PANEL_SIDE_SIZE,\
+											$Constants.SOLAR_PANEL_SIDE_SIZE));
 		new_solar_panel.position = coord/TABLE_RATIO;
 		add_child(new_solar_panel);
+	for coord: Vector2i in $Constants.PLANTS:
+		for i in range(6):
+			var arg = i*PI/3;
+			var new_plant: Sprite2D = plant_scene.instantiate();
+			rescale(new_plant, Vector2i($Constants.POT_RADIUS, $Constants.POT_RADIUS));
+			new_plant.position = (coord + Vector2i($Constants.POT_SPOT_RADIUS*3/4 *\
+					Vector2(cos(arg), sin(arg))))*1.0 / TABLE_RATIO;
+			add_child(new_plant);
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print(TABLE_RATIO)
 	place_elts();
 
 
