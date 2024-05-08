e09c88847577a27249a1251ad0656942a45a2135
Justasimplemantryingtomakeiswayacross <161010453+Justasimplemantryingtomakeiswayacross@users.noreply.github.com>
2024-05-08 22:32:34 +0200

    ✨old main

--- pami_embedded_program/src/main.cpp
+++ pami_embedded_program/src/main.cpp
@@ -1,23 +1,30 @@
 #include <Arduino.h>
 #include "TickTwo.h"
 #include "motor.h"
-#include "captor.h"
+//#include "captor.h"
 
-const int Pin1m1 = 5;                       const int Pin1m2 = 19; 
-const int Pin2m1 = 18;                      const int Pin2m2 = 21;
-const int Pin3m1 = 2;                       const int Pin3m2 = 4;
+const int Pin1mg = 5;                       
+const int Pin1md = 19; 
+const int Pin2mg = 18;                      
+const int Pin2md = 21;
+const int Pin3mg = 2;                       
+const int Pin3md = 4;
 
-const int Pin1c1 =27;                          const int Pin1c2 =24;
-const int Pin2c1 =26;                          const int Pin2c2 =33;
+const int Pin1c1 =27;                          
+const int Pin1c2 =24;
+const int Pin2c1 =26;                          
+const int Pin2c2 =33;
 
 
-int k = 0;
+int e = 0;
+int e1 = 0;
+int e2 =0;
 
-Motor MotorL(Pin1m1, Pin2m1, Pin3m1);       
-Motor MotorR(Pin1m2, Pin2m2, Pin3m2); 
+Motor MotorL(Pin1md, Pin2md, Pin3md);       
+Motor MotorR(Pin1mg, Pin2mg, Pin3mg); 
 
-Captor CaptorL(Pin1c1,Pin2c1);
-Captor CaptorR(Pin1c2,Pin2c2);
+//Captor CaptorL(Pin1c1,Pin2c1);
+//Captor CaptorR(Pin1c2,Pin2c2);
 
 
 uint32_t topsCount_LA = 0;
@@ -51,50 +58,52 @@ int MaxValue = 200;
 int Value;
 
 void trapeze_fonction ();
-void timer_fonction();
+//void timer_fonction();
 void motor_fonction();
 void strat_fonction();
-void sensor_fonction();
-void countTopsL();
-void countTopsR();
+//void sensor_fonction_ass();
+//void sensor_fonction_utile();
+//void countTopsL();
+//void countTopsR();
 
 
 TickTwo ticker  (trapeze_fonction, intervalTemp, 0, MILLIS);
 TickTwo motor   (motor_fonction, intervalTemp,0 , MILLIS);
 TickTwo strat   (strat_fonction, intervalTemp, 0, MILLIS);
-TickTwo sensor  (sensor_fonction, intervalTemp, 0, MILLIS);
-TickTwo timer (timer_fonction, intervalTemp/100, 0, MILLIS);
+//TickTwo sensor  (sensor_fonction_ass, intervalTemp, 0, MILLIS);
+//TickTwo timer (timer_fonction, intervalTemp/100, 0, MILLIS);
 
 void setup() {
 
-  pinMode(Pin1m1, OUTPUT);
-  pinMode(Pin2m1, OUTPUT);
-  pinMode(Pin3m1, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(Pin1c1), countTopsL, RISING);
+  pinMode(Pin1md, OUTPUT);
+  pinMode(Pin2md, OUTPUT);
+  pinMode(Pin3md, OUTPUT);
+  //attachInterrupt(digitalPinToInterrupt(Pin1c1), countTopsL, RISING);
   //attachInterrupt(digitalPinToInterrupt(Pin1c2), countTopsR, RISING);
   flag.Running = false;
-  flag.avancer = true;
-  flag.sens = true;
   flag.enable_ticker = true;
   Serial.begin(9600);
   motor.start();
   strat.start();
-  timer.start();
-  sensor.start();
+  //timer.start();
+  //sensor.start();
 }
 
 void loop() {
   ticker.update();
   motor.update();
   strat.update();
-  timer.update();
-  sensor.update();
-  Serial.printf("%d SpeedL , %d SpeedR, %d time \n", SpeedL, SpeedR, Time_Timer);
+  //timer.update();
+  //sensor.update();
+  //Serial.printf("%d value, %d k\n", Value, k);
   //Serial.printf("%d value ", Value);
   //Serial.printf("%d Running ", flag.Running);
   //Serial.printf("%d time_trapeze \n", Time_Trapeze);
   //Serial.printf("%d speedleft     ", SpeedreadL);
   //Serial.printf("%d speedright \n", SpeedreadR);
+  if(e1){Serial.printf("error flag.avancer=true/flag.sens\n");}
+  if(e2){Serial.printf("error flag.avancer=false/flag.sens\n");}
+  if(e){Serial.printf("error flag.avancer\n");}
 
 }
 
@@ -133,66 +142,114 @@ void motor_fonction()
 {
   if (flag.enable_ticker)
   {
-    if (flag.avancer)
+    if (flag.avancer==true)
     {
+      if (flag.sens == true){
+        analogWrite(Pin3md, Value);
+        digitalWrite(Pin1md, HIGH);
+        digitalWrite(Pin2md, LOW);
       
-        MotorL.setPwm(Value, flag.sens);
-        MotorR.setPwm(Value, flag.sens);
+        analogWrite(Pin3mg, Value);
+        digitalWrite(Pin1mg, HIGH);
+        digitalWrite(Pin2mg, LOW);
+      }
+      else if (flag.sens==false){
+        analogWrite(Pin3md, Value);
+        digitalWrite(Pin1md, LOW);
+        digitalWrite(Pin2md, HIGH);
+
+        analogWrite(Pin3mg, Value/2);
+        digitalWrite(Pin1mg, LOW);
+        digitalWrite(Pin2mg, HIGH);
+      }
+      else{
+        e1=1;
+      }
+    }   
+    
+    else if (flag.avancer==false)
+    {
+      if (flag.sens==true){
+        analogWrite(Pin3m1, Value);
+        digitalWrite(Pin1m1, HIGH);
+        digitalWrite(Pin2m1, LOW);
+      
+        analogWrite(Pin3m2, Value);
+        digitalWrite(Pin2m1, LOW);
+        digitalWrite(Pin2m2, HIGH);
+      }
+      else if (flag.sens==false){
+        analogWrite(Pin3m1, Value);
+        digitalWrite(Pin1m1, LOW);
+        digitalWrite(Pin1m2, HIGH);
+        
+        analogWrite(Pin3m2, Value);
+        digitalWrite(Pin2m1, HIGH);
+        digitalWrite(Pin2m2, LOW);
+      }
+      else{
+        e2=1;
+      }
+    }
+    else{
+      e=1;
     }
   }
-  else
-  {
-      MotorL.setPwm(Value, not flag.sens);
-      MotorR.setPwm(Value, flag.sens);
-    }
 }
+
 
 void strat_fonction(){
 
-  if (not flag.Running && k == 0)
+  if (not flag.Running)
   {
-      flag.avancer =  true;
+      flag.avancer = true;
       flag.sens = false;
       ticker.start();
-      k = 1;
-
+      
   } 
-  else if (not flag.Running && k == 1)
-  {
-      flag.avancer =  true;
-      flag.sens = true;
-      ticker.start();
-      k = 0;
-  }
+  // else if (not flag.Running && k == 1)
+  // {
+  //     flag.avancer =  true;
+  //     flag.sens = true;
+  //     ticker.start();
+  //     k = 0;
+  // }
   
 }
 
-void sensor_fonction(){
-  if (Time_Timer> 10){
-    SpeedL = topsCount_LA/Time_Timer;
-    SpeedR = topsCount_RA/Time_Timer;
-    if (topsValue_LB){
-      SpeedL = -1*SpeedL;
-    }
-    if (topsValue_RB){
-      SpeedR = -1*SpeedR;
-    }
-    Time_Timer = 0;
-    topsCount_LA = 0;
-    topsCount_RA = 0;
-  }
-}
+// void sensor_fonction_ass(){
+//   if (Time_Timer> 10){
+//     SpeedL = topsCount_LA/Time_Timer;
+//     SpeedR = topsCount_RA/Time_Timer;
+//     if (topsValue_LB){
+//       SpeedL = -1*SpeedL;
+//     }
+//     if (topsValue_RB){
+//       SpeedR = -1*SpeedR;
+//     }
+//     Time_Timer = 0;
+//     topsCount_LA = 0;
+//     topsCount_RA = 0;
+//   }
+//}
 
-void countTopsL() {
-  topsCount_LA ++;
-  topsValue_LB = digitalRead(Pin2c1);
-}
+// void sensor_fonction_utile(){
+//   if (not flag.Running){
+//     topsCount_LA = 0;
+//   }
 
-void countTopsR() {
-  topsCount_RA ++;
-  topsValue_RB = digitalRead(Pin2c2);
-}
+// }
 
-void timer_fonction(){
-  Time_Timer++;
-}
+// void countTopsL() {
+//   topsCount_LA ++;
+//   topsValue_LB = digitalRead(Pin2c1);
+// }
+
+// void countTopsR() {
+//   topsCount_RA ++;
+//   topsValue_RB = digitalRead(Pin2c2);
+// }
+
+// void timer_fonction(){
+//   Time_Timer++;
+// }
